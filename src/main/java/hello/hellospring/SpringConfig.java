7c985844f19b8449cde6661d8ead3b24942d9172
ro-el-c <chung3286@naver.com>
2023-01-07 00:16:29 +0900

    6. Spring DataBase Access Technology - pure Jdbc

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,24 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+    private final DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) { // 스프링 부트에서 application.properties 를 바탕으로 스프링 빈 만듦
+        this.dataSource = dataSource;
+    }
+
     @Bean // 스프링 빈으로 등록
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -15,6 +26,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();// MemoryMemberRepository 가 MemberRepository 의 구현체 - 인터페이스는 new 가 안 됨.
+        // return new MemoryMemberRepository();// MemoryMemberRepository 가 MemberRepository 의 구현체 - 인터페이스는 new 가 안 됨.
+        return new JdbcMemberRepository(dataSource);
     }
 }
