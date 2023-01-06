3c5c381dc394d34b8a04313e13027906b202892a
ro-el-c <chung3286@naver.com>
2023-01-06 20:26:55 +0900

    4. Spring Bean & Dependency - Java Code -> Spring Bean

---
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -0,0 +1,20 @@
+package hello.hellospring;
+
+import hello.hellospring.repository.MemberRepository;
+import hello.hellospring.repository.MemoryMemberRepository;
+import hello.hellospring.service.MemberService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean // 스프링 빈으로 등록
+    public MemberService memberService(){
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository(){
+        return new MemoryMemberRepository();// MemoryMemberRepository 가 MemberRepository 의 구현체 - 인터페이스는 new 가 안 됨.
+    }
+}
--- src/main/java/hello/hellospring/service/MemberService.java
+++ src/main/java/hello/hellospring/service/MemberService.java
@@ -9,11 +9,11 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
-@Service
+
 public class MemberService {
     private  MemberRepository memberRepository = new MemoryMemberRepository();
 
-    @Autowired
+
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
